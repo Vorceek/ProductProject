2cf0a8fe0fb57b430b12a89806e380de52e02113
Vorceek <vorceek@gmail.com>
2026-02-24 11:34:32 -0300

    Adicionado novos campos a entidade Produto

--- ProductProject.Domain/Entidades/Produto.cs
+++ ProductProject.Domain/Entidades/Produto.cs
@@ -1,11 +1,18 @@
-﻿namespace ProductProject.Entities
+﻿using System.Data.SqlTypes;
+
+namespace ProductProject.Entities
 {
     public class Produto
     {
         public Guid Id { get; set; }
         public required string Nome { get; set; }
         public required string Descricao { get; set; }
+
         public int Quantidade { get; set; }
+        public int? QuantidadeMinima { get; set; }
+
+        public string? codigoSKU { get; set; }
+        public decimal Preco { get; set; }
 
         public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
         public required Guid CriadoPorId { get; set; }
@@ -14,5 +21,7 @@
         public DateTime? ModificadoEm { get; set; }
         public Guid? ModificadoPorId { get; set; }
         public Usuario? ModificadoPor { get; set; }
+
+        public bool Ativo { get; set; } = true;
     }
 }
