426edeac1a822f30c2d13b3a7ff37e7e8e96499c
Vorceek <vorceek@gmail.com>
2026-02-24 11:41:50 -0300

    Nova entidade Movimentacao

---
+++ ProductProject.Domain/Entidades/Movimentacao.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductProject.Domain.Entidades
+{
+    public enum Tipos
+    {
+        Entrada,
+        Saida
+    }
+
+    public class Movimentacao
+    {
+        public Guid Id { get; set; }
+        public Guid ProdutoId { get; set; }
+        public Guid UsuarioId { get; set; }
+        public Tipos Tipo { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime Data { get; set; }
+        public string Observacao { get; set; } = string.Empty;
+    }
+}
